913cfd03af3f7084c4867b36673424e1c3c21b18
chenyan <253604213@qq.com>
2021-01-07 22:21:27 +0800

    model for my sEMG Graph network

--- gcn.py
+++ gcn.py
@@ -11,7 +11,6 @@ class TimeBlock(nn.Module):
     Neural network block that applies a temporal convolution to each node of
     a graph in isolation.
     """
-
     def __init__(self, in_channels, out_channels, kernel_size=3):
         """
         conv of each node in each time step
@@ -40,14 +39,14 @@ class TimeBlock(nn.Module):
 
 
 class BasicGcn(nn.Module):
-    def __init__(self, A, input_szie, out_szie):
+    def __init__(self, node_size, input_szie, out_szie):
         '''
         :param A: adjustment matrix
         :param input_szie: input dim of node feature
         :param out_szie: output dim of node feature
         '''
         super(BasicGcn, self).__init__()
-        self.weight = nn.Parameter(torch.FloatTensor(A.shape[0], A.shape[1]))
+        self.weight = nn.Parameter(torch.FloatTensor(node_size, node_size))
         self.feature_weight = nn.Parameter(torch.FloatTensor(input_szie, out_szie))
         self.A_norm = self.get_normalized_adj()
         self.reset_parameters()
@@ -71,16 +70,61 @@ class BasicGcn(nn.Module):
         return A_wave
 
     def forward(self, X, A):
-        A_hat=self.A_norm(A)
+        A_hat = self.A_norm(A)
         adj = self.weight*A_hat
         feature = torch.mul(X,self.feature_weight)
         out=torch.mm(adj,feature)
         return F.relu(out), adj
 
-class LSTM(nn.Module):
-    def __init__(self):
-        super (LSTM, self).__init__()
 
+class STGCNBlock(nn.Module):
+    """
+    Neural network block that applies a temporal convolution on each node in
+    isolation, followed by a graph convolution, followed by another temporal
+    convolution on each node.
+    """
+
+    def __init__(self, in_channels, spatial_channels, out_channels,
+                 num_nodes):
+        """
+        :param in_channels: Number of input features at each node in each time
+        step.
+        :param spatial_channels: Number of output channels of the graph
+        convolutional, spatial sub-block.
+        :param out_channels: Desired number of output features at each node in
+        each time step.
+        :param num_nodes: Number of nodes in the graph.
+        """
+        super(STGCNBlock, self).__init__()
+        self.temporal1 = TimeBlock(in_channels=in_channels,
+                                   out_channels=out_channels)
+        self.Theta1 = nn.Parameter(torch.FloatTensor(out_channels,
+                                                     spatial_channels))
+        self.temporal2 = TimeBlock(in_channels=spatial_channels,
+                                   out_channels=out_channels)
+        self.gcn = BasicGcn(num_nodes, in_channels, out_channels)
+        self.batch_norm = nn.BatchNorm2d(num_nodes)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.Theta1.shape[1])
+        self.Theta1.data.uniform_(-stdv, stdv)
+
+    def forward(self, X, A_hat):
+        """
+        :param X: Input data of shape (batch_size, num_nodes, num_timesteps,
+        num_features=in_channels).
+        :param A_hat: Normalized adjacency matrix.
+        :return: Output data of shape (batch_size, num_nodes,
+        num_timesteps_out, num_features=out_channels).
+        """
+        t = self.temporal1(X)
+        t2, adj = self.gcn(A_hat, t)
+        # lfs = torch.einsum("ij,jklm->kilm", [A_hat, t.permute(1, 0, 2, 3)])
+        # # t2 = F.relu(torch.einsum("ijkl,lp->ijkp", [lfs, self.Theta1]))
+        # t2 = F.relu(torch.matmul(lfs, self.Theta1))
+        t3 = self.temporal2(t2)
+        return self.batch_norm(t3), adj
 
 
 if __name__ == '__main__':
