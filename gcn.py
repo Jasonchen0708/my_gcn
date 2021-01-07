b2868ecdc44149fc972d1b2191082f74de31e26a
chenyan <253604213@qq.com>
2021-01-07 20:46:37 +0800

    test

--- gcn.py
+++ gcn.py
@@ -4,3 +4,10 @@ import matplotlib as plt
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.parameter as parameter
+
+
+
+class BasicGcn(nn.Module):
+    def __init__(self,A, input_szie,out_szie):
+        super(BasicGcn, self).__init__()
+
